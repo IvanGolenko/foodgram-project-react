8d22f4b7d6b6b5d177699d8e657f91a84feb5b86
Иван Голенко <igolivanenko1996@gmail.com>
2022-09-05 23:22:40 +0300

    сервер

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='qwerty')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = ['*']
 
